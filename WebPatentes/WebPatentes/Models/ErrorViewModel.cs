62d64adf368a2d3954034294331ec5f0c3d5ae61
Brayan Bonifaz <brayan.bonifaz@gmail.com>
2018-10-05 18:19:30 -0500

    prueba

---
+++ WebPatentes/WebPatentes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebPatentes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
