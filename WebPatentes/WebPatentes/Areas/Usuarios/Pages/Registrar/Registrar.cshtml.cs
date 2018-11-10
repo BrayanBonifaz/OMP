24188fb7469fd7bc7a696bbe9af60232819a104b
USUARIO <USUARIO@MDPFK4F>
2018-11-10 16:14:58 -0600

    n

--- WebPatentes/WebPatentes/Areas/Usuarios/Pages/Registrar/Registrar.cshtml.cs
+++ WebPatentes/WebPatentes/Areas/Usuarios/Pages/Registrar/Registrar.cshtml.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebPatentes.Library;
 
 namespace WebPatentes.Areas.Usuarios.Pages.Registrar
 {
     public class RegistrarModel : PageModel
     {
+        private LUsuarios _usuarios;
         public void OnGet()
         {
+            _usuarios = new LUsuarios();
+            ViewData["Roles"] = _usuarios.userData(HttpContext);
         }
     }
 }
--- WebPatentes/WebPatentes/wwwroot/js/site.js
+++ WebPatentes/WebPatentes/wwwroot/js/site.js
@@ -1,3 +1,9 @@
-﻿$().ready(() => {
+﻿var principal = new Principal();
+
+$().ready(() => {
+
+    let URLactual = window.location.pathname;
+    principal.userLink(URLactual);
+
     $('.sidenav').sidenav();
 });
